f0694a10787be66594bd39e50b126a16b1c2f13a
MasonN808 <mason.naka@gmail.com>
2022-08-16 10:39:53 -1000

    Redoing the for loop initialization during testing

--- src/tests/dependent_for_loop/main.py
+++ src/tests/dependent_for_loop/main.py
@@ -36,23 +36,11 @@ if __name__ == "__main__":
     # Create a DAG manually for the second-order metareasoning problem (inner subtree)
     # ----------------------------------------------------------------------------------------
 
-    # Intermediate Nodes
-    node_inner_5 = Node(5, [], [], expression_type="contract", in_subtree=True)
-    node_inner_4 = Node(4, [node_inner_5], [], expression_type="contract", in_subtree=True)
-    node_inner_3 = Node(3, [node_inner_4], [], expression_type="contract", in_subtree=True)
-    node_inner_2 = Node(2, [node_inner_3], [], expression_type="contract", in_subtree=True)
-
-    # Root node
-    node_inner_1 = Node(1, [node_inner_2], [], expression_type="contract", in_subtree=True)
-
-    # Append the children
-    node_inner_5.children = [node_inner_4]
-    node_inner_4.children = [node_inner_3]
-    node_inner_3.children = [node_inner_2]
-    node_inner_2.children = [node_inner_1]
+    # Root Node
+    node_inner_1 = Node(1, [], [], expression_type="contract", in_subtree=True)
 
     # Create a list of the nodes in breadth-first order for the false branch
-    nodes_inner = [node_inner_1, node_inner_2, node_inner_3, node_inner_4, node_inner_5]
+    nodes_inner = [node_inner_1]
 
     # ----------------------------------------------------------------------------------------
     # Create a DAG manually for the first-order metareasoning problem
@@ -78,30 +66,20 @@ if __name__ == "__main__":
     # ----------------------------------------------------------------------------------------
 
     # Leaf node
-    node_7 = Node(7, [], [], expression_type="contract", in_subtree=False)
+    node_2 = Node(2, [], [], expression_type="contract", in_subtree=False)
 
     # Intermediate Nodes
-    node_6 = Node(6, [node_7], [], expression_type="for", in_subtree=False)
-    node_5 = Node(5, [node_6], [], expression_type="contract", in_subtree=True)
-    node_4 = Node(4, [node_5], [], expression_type="contract", in_subtree=True)
-    node_3 = Node(3, [node_4], [], expression_type="contract", in_subtree=True)
-    node_2 = Node(2, [node_3], [], expression_type="contract", in_subtree=True)
-    node_1 = Node(1, [node_2], [], expression_type="contract", in_subtree=False)
+    node_1 = Node(1, [node_2], [], expression_type="for", in_subtree=False)
 
     # Root Node
     node_root = Node(0, [node_1], [], expression_type="contract", in_subtree=False)
 
     # Append the children
-    node_7.children = [node_6]
-    node_6.children = [node_5]
-    node_5.children = [node_4]
-    node_4.children = [node_3]
-    node_3.children = [node_2]
     node_2.children = [node_1]
     node_1.children = [node_root]
 
     # For a list of nodes for the DAG creation
-    nodes = [node_root, node_1, node_2, node_3, node_4, node_5, node_6, node_7]
+    nodes = [node_root, node_1, node_2]
 
     program_dag = DirectedAcyclicGraph(nodes, node_root)
 
@@ -127,7 +105,7 @@ if __name__ == "__main__":
         # Need to initialize it after adjusting program_dag
         # A higher number x indicates a higher velocity in f(x)=1-e^{-x*t}
         # Note that the numbers can't be too small; otherwise the qualities converge to 0, giving a 0 utility
-        generator.manual_override = [10000, 0.1, 0.1, 0.1, 0.1, 0.1, 10000, "simple_conditional", 10000]
+        generator.manual_override = [.1, "for", .1]
 
         # Generate the nodes' quality mappings
         nodes = generator.generate_nodes()  # Return a list of file names of the nodes
@@ -150,31 +128,20 @@ if __name__ == "__main__":
     for_subtree = DirectedAcyclicGraph(nodes_inner, root=node_inner_1)
 
     # Convert to a contract program
-    node_6.for_subprogram = ContractProgram(program_id=1, parent_program=program_outer, child_programs=None, program_dag=for_subtree, budget=0, scale=10 ** 6, decimals=3,
+    node_1.for_subprogram = ContractProgram(program_id=1, parent_program=program_outer, child_programs=None, program_dag=for_subtree, budget=0, scale=10 ** 6, decimals=3,
                                             quality_interval=QUALITY_INTERVAL,
                                             time_interval=TIME_INTERVAL, time_step_size=TIME_STEP_SIZE, in_subtree=True, generator_dag=program_dag)
 
     # Initialize the pointers of the nodes to the program it is in
-    initialize_node_pointers_current_program(node_6.true_subprogram)
+    initialize_node_pointers_current_program(node_1.for_subprogram)
 
-    program_outer.child_programs = [node_6.for_subprogram]
+    program_outer.child_programs = [node_1.for_subprogram]
 
     # Add the pointers from the parent program to the subprograms
-    node_6.for_subprogram.parent_program = program_outer
-
-    # Add the pointers from the generator dag to the subprograms
-    node_6.false_subprogram.generator_dag = program_dag
+    node_1.for_subprogram.parent_program = program_outer
 
     # The input should be the outermost program
     test = Test(program_outer)
 
-    # Print the tree for verification
-    # print(test.print_tree(program_dag.root))
-
-    # Test a random distribution on the initial allocations
-    # print(test.test_initial_allocations(iterations=500, initial_is_random=True, verbose=False))
-
     # Test initial vs optimal expected utility and allocations
     test.find_utility_and_allocations(initial_allocation="uniform", outer_program=program_outer, verbose=True)
-    # test.find_utility_and_allocations(initial_allocation="uniform with noise", outer_program=program_outer, verbose=False)
-    # test.find_utility_and_allocations(initial_allocation="Dirichlet", outer_program=program_outer, verbose=False)
