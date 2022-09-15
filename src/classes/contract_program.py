89d065b00a3c178de504abe23762a55b51a7af0d
MasonN808 <mason.naka@gmail.com>
2022-09-15 18:53:33 -0400

    Renamed the exact EU method.  Start of restructuring the EU for the entire contract program

--- src/classes/contract_program.py
+++ src/classes/contract_program.py
@@ -260,7 +260,7 @@ class ContractProgram:
                 # print(node.id)
                 # quit()
 
-                return self.find_exact_expected_utility_2(time_allocations=time_allocations, possible_qualities=self.possible_qualities, expected_utility=1,
+                return self.find_exact_expected_utility(time_allocations=time_allocations, possible_qualities=self.possible_qualities, expected_utility=1,
                                                           current_qualities=[None for i in range(self.number_of_loops)], parent_qualities=parent_qualities,
                                                           depth=0, leafs=[node], prev_conditional_prob=None, sum=0)
 
@@ -364,7 +364,7 @@ class ContractProgram:
 
     #     return sum
 
-    def find_exact_expected_utility_2(self, leafs, time_allocations, depth, expected_utility, current_qualities, parent_qualities, possible_qualities, prev_conditional_prob, sum) -> float:
+    def find_exact_expected_utility(self, leafs, time_allocations, depth, expected_utility, current_qualities, parent_qualities, possible_qualities, prev_conditional_prob, sum) -> float:
         """
         Returns the parent qualities given the time allocations and node
 
@@ -421,7 +421,7 @@ class ContractProgram:
 
                     else:
 
-                        expected_utility += conditional_probability * self.find_exact_expected_utility_2(leafs=new_leafs, time_allocations=time_allocations, depth=depth,
+                        expected_utility += conditional_probability * self.find_exact_expected_utility(leafs=new_leafs, time_allocations=time_allocations, depth=depth,
                                                                                                          expected_utility=expected_utility, current_qualities=current_qualities,
                                                                                                          possible_qualities=possible_qualities, parent_qualities=[],
                                                                                                          prev_conditional_prob=conditional_probability, sum=0)
