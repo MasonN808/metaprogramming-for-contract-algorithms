Box plots need work on taking average in list. Bug in RHC after adding noise to PP

src/classes/generator.py
@@ -81,8 +81,19 @@ class Generator:
                     velocity = self.parent_dependent_transform(node, qualities, random_number)
 
                     for t in np.arange(0, self.time_limit + self.time_step_size, self.time_step_size).round(utils.find_number_decimals(self.time_step_size)):
+                        # Add some noise to the qualities for each instance
+                        noise = np.random.normal(loc=0, scale=.025)
+
+                        generated_quality = (1 - math.e ** (-velocity * t)) + noise
+
+                        # Check bounds and truncate
+                        if generated_quality < 0:
+                            generated_quality = 0
+                        if generated_quality > 1:
+                            generated_quality = 1
+                            
                         # Use this function to approximate the performance profile
-                        dictionary[quality][t] = 1 - math.e ** (-velocity * t)
+                        dictionary[quality][t] = generated_quality
                 else:
                     qualities.append(quality)
                     self.recur_build(depth + 1, node, qualities, dictionary[quality], random_number)
@@ -97,13 +108,14 @@ class Generator:
                 if qualities:
                     # Convert the list of strings to floats
                     qualities = [float(quality) for quality in qualities]
-
                     # Get the average parent quality (this may not be what we want)
                     average_parent_quality = sum(qualities) / len(node.parents)
+                    print("Sum: {} -- Average: {} -- # Parents: {} -- NODE ID: {}".format(sum(qualities), average_parent_quality, len(node.parents), node.id))
 
                 return self.manual_override[node.id] * average_parent_quality
 
             else:
+                print("BAD")
                 return random_number
 
         else:
@@ -142,10 +154,11 @@ class Generator:
         :return: dictionary
         """
         dictionary = {'instances': {}}
-        # Take a random value from a uniform distribution; used for nodes without parents
-        c = np.random.uniform(low=self.uniform_low, high=self.uniform_high)
 
         for i in range(self.instances):
+            # Take a random value from a uniform distribution; used for nodes without parents
+            c = np.random.uniform(low=self.uniform_low, high=self.uniform_high)
+
             # Add some noise to the random value
             c = c + abs(np.random.normal(loc=0, scale=.05))  # loc is mean; scale is st. dev.
 

src/tests/complex_conditional_and_loop_main/main.py
@@ -215,10 +215,10 @@ if __name__ == "__main__":
     nodes = [root, node_1, node_2, node_3, node_4, node_5, node_6, node_7, node_8, node_9, node_10, node_11, node_12, node_13, node_14]
     program_dag = DirectedAcyclicGraph(nodes, root)
 
-    iterations = 1
+    ITERATIONS = 1
 
     # Use a Dirichlet distribution to generate random ppvs
-    performance_profile_velocities = utils.dirichlet_ppv(iterations=iterations, dag=program_dag, alpha=.9, constant=10)
+    performance_profile_velocities = utils.dirichlet_ppv(iterations=ITERATIONS, dag=program_dag, alpha=.9, constant=10)
 
     # Plot types:
     #   - "box_whisker" => a box and whisker plot of EU for our contract program on differing solution methods
@@ -233,8 +233,14 @@ if __name__ == "__main__":
     plot_nodes = [4, 8, 11]
     plot_methods = "subset"
 
-    eu_list = [[] for i in range(0, 13)]
-    time_list = [[] for i in range(0, 13)]
+    NUM_METHODS = 0
+    if (plot_methods == "subset"):
+        NUM_METHODS = 5
+    elif (plot_methods == "all"):
+        NUM_METHODS = 13
+
+    eu_list = [[] for i in range(0, NUM_METHODS)]
+    time_list = [[] for i in range(0, NUM_METHODS)]
 
     for ppv in performance_profile_velocities:
         # Used to create the synthetic data as instances and a populous file
@@ -336,11 +342,11 @@ if __name__ == "__main__":
                 # Test solution method expected utilities and allocations
                 eu_time = test.find_utility_and_allocations(initial_allocation="uniform", outer_program=program_outer, verbose=False)
                 # Append the EUs appropriately to list in outer scope
-                for index in range(0, 13):
+                for index in range(0, NUM_METHODS):
                     eu_list[index].append(eu_time[0][index])
             elif (plot_methods == "subset"):
-                eu_time = test.find_utility_and_allocations(initial_allocation="uniform", outer_program=program_outer, verbose=False)
-                for index in range(0, 5):
+                eu_time = test.find_utility_and_allocations(initial_allocation="uniform", outer_program=program_outer, verbose=True)
+                for index in range(0, NUM_METHODS):
                     eu_list[index].append(eu_time[0][index])
 
         elif (plot_type == "bar"):
@@ -348,17 +354,19 @@ if __name__ == "__main__":
                 # Test solution method expected utilities and allocations
                 eu_time = test.find_utility_and_allocations(initial_allocation="uniform", outer_program=program_outer, verbose=False)
                 # Append the EUs appropriately to list in outer scope
-                for index in range(0, 13):
+                for index in range(0, len(plot_nodes)):
+                    # Join the lists rather than appending
                     time_list[index].append(eu_time[1][index])
-                pass
             elif (plot_methods == "subset"):
                 eu_time = test.find_utility_and_allocations(initial_allocation="uniform", outer_program=program_outer, verbose=False)
-                for index in range(0, 5):
+                print(eu_time[1])
+                for index in range(0, len(plot_nodes)):
+                    # Join the lists rather than appending
                     time_list[index].append(eu_time[1][index])
 
     # print(eu_list)
 
-    FILENAME = '{}-{}-{}.png'.format(plot_type, plot_methods, iterations)
+    FILENAME = '{}-{}-iterations{}.png'.format(plot_type, plot_methods, ITERATIONS)
 
     if (plot_type == "box_whisker"):
         if (plot_methods == "all"):
@@ -432,68 +440,81 @@ if __name__ == "__main__":
     elif (plot_type == "bar"):
         if (plot_methods == "all"):
             # Plot results
-            proportional1 = np.array(eu_list[0])
-            proportional2 = np.array(eu_list[1])
-            proportional3 = np.array(eu_list[2])
-            proportional4 = np.array(eu_list[3])
-            proportional5 = np.array(eu_list[4])
-            proportional6 = np.array(eu_list[5])
-            proportional7 = np.array(eu_list[6])
-            proportional8 = np.array(eu_list[7])
-            proportional9 = np.array(eu_list[8])
-            proportional10 = np.array(eu_list[9])
-            proportional11 = np.array(eu_list[10])
+            for node_index in range(0, len(plot_nodes)):
+                FILENAME = 'bar_charts/{}-{}-iterations{}-node{}.png'.format(plot_type, plot_methods, ITERATIONS, plot_nodes[node_index])
+                # Plot results
+                proportional1 = np.array(time_list[0])
+                proportional2 = np.array(time_list[1])
+                proportional3 = np.array(time_list[2])
+                proportional4 = np.array(time_list[3])
+                proportional5 = np.array(time_list[4])
+                proportional6 = np.array(time_list[5])
+                proportional7 = np.array(time_list[6])
+                proportional8 = np.array(time_list[7])
+                proportional9 = np.array(time_list[8])
+                proportional10 = np.array(time_list[9])
+                proportional11 = np.array(time_list[10])
 
-            uniform = np.array(eu_list[11])
-            ehc = np.array(eu_list[12])
+                uniform = np.array(time_list[11])
+                ehc = np.array(time_list[12])
 
-            figure = plt.figure(figsize=(12, 6))
+                figure = plt.figure(figsize=(12, 6))
 
-            plt.title("Expected Utility Variation on Solution Methods")
-            plt.ylabel("Expected Utility")
-            plt.xlabel("Solution Methods")
+                plt.title("Expected Utility Variation on Solution Methods")
+                plt.ylabel("Expected Utility")
+                plt.xlabel("Solution Methods")
 
-            plt.boxplot([proportional1, proportional2, proportional3, proportional4, proportional5, proportional6, proportional7, proportional8, proportional9, proportional10, proportional11, uniform, ehc])
-            plt.xticks([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], ['PA (ß=1)', 'PA (ß=.9)', 'PA (ß=.8)', 'PA (ß=.7)', 'PA (ß=.6)', 'PA (ß=.5)', 'PA (ß=.4)', 'PA (ß=.3)', 'PA (ß=.2)', 'PA (ß=.1)', 'PA (ß=0)', 'Uniform', 'EHC'])
+                plt.bar([proportional1, proportional2, proportional3, proportional4, proportional5, proportional6, proportional7, proportional8, proportional9, proportional10, proportional11, uniform, ehc])
+                plt.xticks([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], ['PA (ß=1)', 'PA (ß=.9)', 'PA (ß=.8)', 'PA (ß=.7)', 'PA (ß=.6)', 'PA (ß=.5)', 'PA (ß=.4)', 'PA (ß=.3)', 'PA (ß=.2)', 'PA (ß=.1)', 'PA (ß=0)', 'Uniform', 'EHC'])
 
-            plt.rcParams["font.family"] = "Times New Roman"
-            plt.rcParams["font.size"] = 11
-            plt.rcParams["grid.linestyle"] = "-"
-            plt.grid(True)
+                plt.rcParams["font.family"] = "Times New Roman"
+                plt.rcParams["font.size"] = 11
+                plt.rcParams["grid.linestyle"] = "-"
+                plt.grid(True)
 
-            axis = plt.gca()
-            axis.spines["top"].set_visible(False)
+                axis = plt.gca()
+                axis.spines["top"].set_visible(False)
 
-            plt.tight_layout()
-            figure.savefig(FILENAME)
-            plt.show()
+                plt.tight_layout()
+                figure.savefig(FILENAME)
 
         else:
+            # Get the average time across all instances
+            total = [0 for i in range(0, NUM_METHODS)]
+            for method_index in range(0, NUM_METHODS):
+                for iteration in range(0, ITERATIONS):
+                    for sublist in time_list[iteration]:
+                        print(sublist[method_index])
+                        total[method_index] += sublist[method_index]
+
+            average_times = [total_time/ITERATIONS for total_time in total]
+
             # Plot results
-            proportional1 = np.array(eu_list[0])
-            proportional2 = np.array(eu_list[1])
-            proportional3 = np.array(eu_list[2])
+            for node_index in range(0, len(plot_nodes)):
+                FILENAME = 'bar_charts/{}-{}-iterations{}-node{}.png'.format(plot_type, plot_methods, ITERATIONS, plot_nodes[node_index])
+                proportional1 = np.array(average_times[node_index][0])
+                proportional2 = np.array(average_times[node_index][1])
+                proportional3 = np.array(average_times[node_index][2])
 
-            uniform = np.array(eu_list[3])
-            ehc = np.array(eu_list[4])
+                uniform = np.array(average_times[node_index][3])
+                ehc = np.array(average_times[node_index][4])
 
-            figure = plt.figure(figsize=(12, 6))
+                figure = plt.figure(figsize=(12, 6))
 
-            plt.title("Expected Utility Variation on Solution Methods")
-            plt.ylabel("Expected Utility")
-            plt.xlabel("Solution Methods")
+                plt.title("Average Time Allocation on Node {}".format(plot_nodes[node_index]))
+                plt.ylabel("Average Time Allocation")
+                plt.xlabel("Solution Methods")
 
-            plt.boxplot([proportional1, proportional2, proportional3, uniform, ehc])
-            plt.xticks([1, 2, 3, 4, 5], ['PA (ß=1)', 'PA (ß=.5)', 'PA (ß=0)', 'Uniform', 'EHC'])
+                plt.bar(x=['PA (ß=1)', 'PA (ß=.5)', 'PA (ß=0)', 'Uniform', 'EHC'], height=[proportional1, proportional2, proportional3, uniform, ehc])
+                # plt.xticks([1, 2, 3, 4, 5], ['PA (ß=1)', 'PA (ß=.5)', 'PA (ß=0)', 'Uniform', 'EHC'])
 
-            plt.rcParams["font.family"] = "Times New Roman"
-            plt.rcParams["font.size"] = 11
-            plt.rcParams["grid.linestyle"] = "-"
-            plt.grid(True)
+                plt.rcParams["font.family"] = "Times New Roman"
+                plt.rcParams["font.size"] = 11
+                plt.rcParams["grid.linestyle"] = "-"
+                plt.grid(True)
 
-            axis = plt.gca()
-            axis.spines["top"].set_visible(False)
+                axis = plt.gca()
+                axis.spines["top"].set_visible(False)
 
-            plt.tight_layout()
-            figure.savefig(FILENAME)
-            plt.show()
+                plt.tight_layout()
+                figure.savefig(FILENAME)