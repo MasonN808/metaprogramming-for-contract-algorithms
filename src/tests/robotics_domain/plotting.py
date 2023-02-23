a3053e7e91e5d6ca0c65de80ce98ae39925d412c
MasonN808 <mason.naka@gmail.com>
2023-02-23 12:38:12 -0500

    more refactors

--- src/tests/robotics_domain/plotting.py
+++ src/tests/robotics_domain/plotting.py
@@ -103,7 +103,7 @@ def plot(plot_type, node_indicies, subset_methods, file_eus, file_times, file_c_
         plt.show()
 
     elif (plot_type == "bar"):
-        methods_length = len(pickled_time_list[node_index])
+        methods_length = len(pickled_time_list[0])
         total = [[0 for j in range(0, methods_length)] for i in range(0, len(node_indicies))]
         for node_index in range(0, len(node_indicies)):
             for method_index in range(0, methods_length):
@@ -332,5 +332,5 @@ if __name__ == "__main__":
     # subset_methods = ['PA (ß=1)', 'PA (ß=.5)', 'PA (ß=.1)', 'PA (ß=0)', r'\textsc{Equal}', r'\textsc{Rhc}']
     # print_eu_data(file_eus=file_eus, subset_methods=subset_methods)
 
-    plot(plot_type="scatter", node_indicies=node_indicies, subset_methods=subset_methods, c_list=c_list, c_node_id=c_node_id,
+    plot(plot_type="bar", node_indicies=node_indicies, subset_methods=subset_methods, c_list=c_list, c_node_id=c_node_id,
          file_eus=file_eus, file_times=file_times, file_c_times=file_c_times, bar_plot_nodes=[1])
