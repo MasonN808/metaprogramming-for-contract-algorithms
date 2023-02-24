ff6ce40453da3600e32470ed52d8f7c10b424fb3
Justin Svegliato <justin.svegliato@gmail.com>
2023-02-23 22:52:42 -0800

    Styled the EU plot some more

--- src/tests/plotting_eu_monitoring.py
+++ src/tests/plotting_eu_monitoring.py
@@ -5,29 +5,35 @@ plt.style.use('seaborn-v0_8')
 plt.rcParams["font.family"] = "Times New Roman"
 plt.rcParams["font.size"] = 14
 plt.rcParams['text.usetex'] = True
+plt.rcParams['text.latex.preamble'] = r'\usepackage{amsfonts}'
 
 PLOT_TYPE = 'line-eu-monitoring'
 FILENAME = 'src/tests/EU_monitoring/experiment'
 BEST_EUS, NEW_EUS = pickle.load(open('src/tests/large-func/data/eu_monitoring_data.txt', 'rb'))
-x_range = range(0, len(BEST_EUS))
+x_range = range(len(BEST_EUS))
+print(len(BEST_EUS))
 
 
 def plot():
-    # Make best EUs green
+    plt.figure(figsize=(12, 2))
+
     plt.plot(x_range, BEST_EUS, c="seagreen",  marker="o", markersize=1, label="Best EU", zorder=1, linewidth=1.2)
 
-    # Make new EUs blue
-    plt.plot(x_range, NEW_EUS, c="steelblue", marker="o", markersize=1, label="New EU", zorder=2, linewidth=.5)
-
-    plt.legend(loc='upper right', ncol=1, labelspacing=0.8, fontsize=13)
-
-    plt.ylabel("Expected Utility")
-    plt.xlabel("Epochs")
+    plt.ylabel(r"$\mathbb{E}[U_\mathcal{M}(\mathbf{t})]$")
+    plt.xlabel("Attempted Swaps")
 
     axis = plt.gca()
-    plt.setp(axis.get_xticklabels(), fontsize=14)
-    plt.setp(axis.get_yticklabels(), fontsize=14)
-    axis.set_ylim([0, 5])
+
+    plt.setp(axis.get_xticklabels(), fontsize=12)
+    plt.setp(axis.get_yticklabels(), fontsize=12)
+
+    axis.set_ylim([0, 4.5])
+    axis.set_xlim([-40, 3410])
+    axis.set_xticks([])
+    axis.set_yticks([])
+
+    axis.xaxis.set_label_coords(0.93, 0.15)
+    axis.yaxis.set_label_coords(0.021, 0.73)
 
     plt.tight_layout()
     plt.show()
