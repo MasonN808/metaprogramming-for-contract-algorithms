877b64d4f6c7160e3c8097b5c196fcff16dedf77
Justin Svegliato <justin.svegliato@gmail.com>
2023-02-23 20:06:15 -0800

    Fixed a bug in the plotting code

--- src/tests/robotics_domain/bar-graph-plotter.py
+++ src/tests/robotics_domain/bar-graph-plotter.py
@@ -8,24 +8,25 @@ plt.rcParams['text.usetex'] = True
 
 PLOT_TYPE = 'bar-graph'
 SIMULATIONS = 150
-NODES = [
-    {'id': 0, 'label': r'$v_0$'},
-    {'id': 1, 'label': r'$v_1$'},
-    {'id': 2, 'label': r'$v_2$'},
-    {'id': 3, 'label': r'$v_3$'},
-    {'id': 4, 'label': r'$v_4$'},
-    {'id': 5, 'label': r'$v_5$'},
-    {'id': 6, 'label': r'$v_6$'},
-    {'id': 7, 'label': r'$v_7$'},
-    {'id': 8, 'label': r'$v_8$'},
-    {'id': 9, 'label': r'$v_9$'},
-    {'id': 10, 'label': r'$v_{10}$'},
-    {'id': 11, 'label': r'$v_{11}$'},
-    {'id': 12, 'label': r'$v_{12}$'},
-    {'id': 13, 'label': r'$v_{13}$'},
-    {'id': 14, 'label': r'$v_{14}$'}
-]
 TECHNIQUE_INDEX = 12
+BUDGET =  10
+
+NODES = {
+    1: {'index': 1, 'label': r'$v_1$', 'color': 'seagreen'},
+    2: {'index': 2, 'label': r'$v_2$', 'color': 'steelblue'},
+    3: {'index': 3, 'label': r'$v_3$', 'color': 'seagreen'},
+    4: {'index': 4, 'label': r'$v_4$', 'color': 'seagreen'},
+    5: {'index': 5, 'label': r'$v_5$', 'color': 'seagreen'},
+    6: {'index': 6, 'label': r'$v_6$', 'color': 'steelblue'},
+    7: {'index': 8, 'label': r'$v_7$', 'color': 'indianred'},
+    8: {'index': 9, 'label': r'$v_8$', 'color': 'indianred'},
+    9: {'index': 10, 'label': r'$v_9$', 'color': 'orchid'},
+    10: {'index': 11, 'label': r'$v_{10}$', 'color': 'orchid'},
+    11: {'index': 12, 'label': r'$v_{11}$', 'color': 'orchid'},
+    12: {'index': 14, 'label': r'$v_{12}$', 'color': 'indianred'},
+}
+NUM_NODES = 12
+NODE_ORDERING = [12, 11, 10, 9, 8, 7, 5, 4, 3, 1, 6, 2]
 
 TIME_ALLOCATION_FILE = 'src/tests/robotics_domain/data/time_data.txt'
 TIME_ALLOCATIONS = pickle.load(open('src/tests/robotics_domain/data/time_data.txt', 'rb'))
@@ -34,12 +35,16 @@ TIME_ALLOCATIONS.insert(13, {TECHNIQUE_INDEX: [0 for _ in range(SIMULATIONS)]})
 
 
 def plot():    
-    average_time_allocations = [0 for _ in range(len(NODES))]
+    average_time_allocations = [0 for _ in range(NUM_NODES)]
 
-    for index in range(len(NODES)):
+    for id in range(NUM_NODES):
         for simulation in range(SIMULATIONS):
-            average_time_allocations[index] += TIME_ALLOCATIONS[NODES[index]['id']][TECHNIQUE_INDEX][simulation]
-        average_time_allocations[index] /= SIMULATIONS
+            average_time_allocations[id] += TIME_ALLOCATIONS[NODES[id + 1]['index']][TECHNIQUE_INDEX][simulation]
+        average_time_allocations[id] /= SIMULATIONS
+
+    total_time_allocations = sum(average_time_allocations)
+    for i in range(len(average_time_allocations)):
+        average_time_allocations[i] = BUDGET * (average_time_allocations[i] / total_time_allocations)
 
     plt.figure(figsize=(12, 6))
 
@@ -50,7 +55,13 @@ def plot():
     plt.ylabel("Average Time Allocation [sec]")
     plt.xlabel("Vertex")
 
-    plt.bar(x=[node['label'] for node in NODES], height=average_time_allocations)
+    x = [NODES[id]['label'] for id in NODE_ORDERING]
+    height = [average_time_allocations[id - 1] for id in NODE_ORDERING]
+    color = [NODES[id]['color'] for id in NODE_ORDERING]
+    plt.bar(x, height, color=color, hatch='//')
+
+    for i in range(len(x)):
+        plt.text(i, height[i] + 0.01, round(height[i], 2), ha='center', zorder=100)
 
     plt.tight_layout()
     plt.show()
--- src/tests/robotics_domain/plotting.py
+++ src/tests/robotics_domain/plotting.py
@@ -8,6 +8,10 @@ plt.rcParams['text.usetex'] = True
 
 import numpy as np
 
+POSSIBLE_METHODS = ['PA (ß=10)', 'PA (ß=5)', 'PA (ß=4)', 'PA (ß=3)', 'PA (ß=2)', 'PA (ß=1)', 'PA (ß=0.8)', 'PA (ß=0.6)', 'PA (ß=0.5)', 'PA (ß=0.1)', 'PA (ß=0)', r'\textsc{Equal}', r'\textsc{Rhc}']
+
+MAX_NUM_METHODS = len(POSSIBLE_METHODS)
+
 def plot(plot_type, node_indicies, subset_methods, file_eus, file_times, file_c_times, bar_plot_nodes=None, c_list=None, c_node_id=None):
     """
     Plots information pertinent to time allocation and expected utiltiy for various allocation methods
@@ -97,7 +101,7 @@ def plot(plot_type, node_indicies, subset_methods, file_eus, file_times, file_c_
         x_axis = subset_methods
 
         colors = iter(plt.cm.coolwarm(np.linspace(1, 0, 10)))
-        color = ['steelblue'] + [next(colors) for _ in range(10)] + ['seagreen']
+        color = ['slategray'] + [next(colors) for _ in range(10)] + ['seagreen']
 
         i = 0
         for patch in boxplot['boxes']:
@@ -208,7 +212,6 @@ def plot(plot_type, node_indicies, subset_methods, file_eus, file_times, file_c_
             temp_allocations = []
             for method_index in range(0, len(pickled_c_times[0][0])):
                 # Get the allocations for the specified transformed node_id
-                print(method_index)
                 if method_index != 12:
                     temp_allocations.append(pickled_c_times[ppv_index][transformed_node_id][method_index])
                 else:
@@ -266,19 +269,20 @@ def plot(plot_type, node_indicies, subset_methods, file_eus, file_times, file_c_
         figure = plt.figure(figsize=(10, 5))
 
         # Make Uniform gray
-        plt.plot(c_list, times[10], c="steelblue", label=subset_methods[0])
+        plt.plot(c_list, times[0], c="slategray", label=subset_methods[0])
 
         # Make the PA methods a single heatmap color
         colors = iter(plt.cm.coolwarm(np.linspace(1, 0, 10)))
-        for index, method_str in enumerate(subset_methods[1:-1]):
+        for index, method_str in list(enumerate(subset_methods))[1:11]:
+            print(index)
             plt.plot(c_list, times[index], c=next(colors), linestyle='dashed', label=method_str)
 
         # Make RHC green
-        plt.plot(c_list, times[11], c="seagreen", marker="o", label=subset_methods[-1], zorder=100)
+        plt.plot(c_list, times[11], c="seagreen", marker="o", label=subset_methods[11], zorder=100)
 
         plt.legend(loc='upper right', ncol=3, labelspacing=0.8, fontsize=13)
 
-        plt.ylabel("Time Allocation [sec]")
+        plt.ylabel("Average Time Allocation [sec]")
         plt.xlabel("Growth Rate")
 
         axis = plt.gca()
@@ -346,5 +350,5 @@ if __name__ == "__main__":
     # subset_methods = ['PA (ß=1)', 'PA (ß=.5)', 'PA (ß=.1)', 'PA (ß=0)', r'\textsc{Equal}', r'\textsc{Rhc}']
     # print_eu_data(file_eus=file_eus, subset_methods=subset_methods)
 
-    plot(plot_type="bar", node_indicies=node_indicies, subset_methods=subset_methods, c_list=c_list, c_node_id=c_node_id,
+    plot(plot_type="box_whisker", node_indicies=node_indicies, subset_methods=subset_methods, c_list=c_list, c_node_id=c_node_id,
          file_eus=file_eus, file_times=file_times, file_c_times=file_c_times, bar_plot_nodes=[1])
